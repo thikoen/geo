a046bb030256721088a8f2d0ce67b0adc609e8ee
Thilo Koenig <de.koenigthilo@gmail.com>
2022-09-02 00:36:42 +0200

    Corrected docker syntac

--- runGeo.sh
+++ runGeo.sh
@@ -4,9 +4,11 @@
 #you might change postgres mount to your preferences
 echo Downloading image...
 sleep 1
-docker run --name wp -d -p 3000:3000 -v /postgres_db:/var/lib/postgresql/data thkoenia/wahlprojekt:05 && docker exec -d -it wp npm run seed
+
+docker run --name wp -d -p 3000:3000 -v /postgres_db:/var/lib/postgresql/data thkoenia/wahlprojekt:05
+docker exec -d -it wp sh -c "npm run seed"
 
 #starting the application
 echo Starting Geo App.... Reach it under localhost:3000
 sleep 5
-docker exec -d -it wp npm start
+docker exec -d -it wp sh-c "npm start"
